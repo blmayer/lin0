7f13f0037bc4c57d7e777c38036a5451316a3f20
myr <myr@terminal.pink>
2024-09-14 23:36:16 -0300

    added rootfs tarball and updated index.html
    * small changes to make.sh build script

--- make.sh
+++ make.sh
@@ -1,93 +1,105 @@
 #!/bin/sh
 
 set -e
+arch="$(uname -m)"
 
-muslurl="https://musl.libc.org/releases/musl-1.2.5.tar.gz"
-tccurl="https://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2"
-linuxurl="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.4.tar.xz"
-librelslurl="https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.9.2.tar.gz"
-toyboxurl="https://github.com/landley/toybox/archive/refs/heads/master.zip"
+muslver="1.2.5"
+tccver="0.9.27"
+linuxver="6.10.4"
+sslver="3.9.2"
+
+muslurl="https://musl.libc.org/releases/musl-$muslver.tar.gz"
+tccurl="https://mirror.marwan.ma/savannah/tinycc/tcc-$tccver.tar.bz2"
+linuxurl="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$linuxver.tar.xz"
+toyboxurl="https://codeload.github.com/landley/toybox/zip/refs/heads/master"
 mkshurl="http://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-R59c.tgz"
 
-mkdir build
+[ -d build ] || mkdir build
 cd build
 
-echo "building musl..."
-curl -O "$muslurl"
-musl="$(basename "$muslurl")"
-tar xf "$musl"
-cd "${musl%%\.*}"
-./configure --prefix ../../rootfs
-make && make install
-make obj/musl-gcc
-sed -i "s/ld-musl-$arch.so.1/libc.so/" lib/musl-gcc.specs
-CC="$PWD/obj/musl-gcc"
-ln -sv ../../rootfs/lib/libc.so ../../rootfs/bin/ldd
-ln -sv ../../rootfs/lib/libc.so ../../rootfs/bin/ld
-cd ..
+[ -d "musl-$muslver" ] || {
+	echo "building musl..."
+	curl "$muslurl" | tar xz
+	musl="$(basename "$muslurl")"
+	cd "musl-$muslver"
+	./configure --prefix=../../rootfs
+	make && make install
+	make obj/musl-gcc
+	sed -i "s@ld-musl-$arch.so.1@libc.so@" ../../rootfs/lib/musl-gcc.specs
+	cd ..
+}
 
-echo "building tcc..."
-curl -O "$tccurl"
-tar xf "$(basename "$tccurl")"
-cd tinycc-mob
-CC="$CC -static -no-pie" ./configure --prefix=/home/lord/distro/rootfs --enable-static --config-musl --elfinterp=/lib/libc.so --sysincludepaths=/include --libpaths=/lib --crtprefix=/lib
-make && make install
-cat << EOF > ../../rootfs/bin/ar
-#!/bin/sh
-cc -ar $@
-EOF
-cd ..
+export CC="$(realpath ../rootfs/bin/musl-gcc)"
+
+[ -d "tcc-$tccver" ] || {
+	echo "building tcc..."
+	curl "$tccurl" | tar xj
+	cd "tcc-$tccver"
+	./configure --prefix=/home/lord/git/lin0/rootfs --cc="$CC -static -no-pie" --enable-static --config-musl --elfinterp=/lib/libc.so --sysincludepaths=/include --libpaths=/lib --crtprefix=/lib
+	make && make install
+	cat <<- EOF > ../../rootfs/bin/ar
+	#!/bin/sh
+	cc -ar $@
+	EOF
+	cd ..
+}
 
 # TODO: test
 # echo "testing tcc..."
 
-echo "installing linux headers..."
-case "$platform" in
-	"rpi"*) linuxurl="$rpilinuxurl" ;;
-esac
-curl -O "$linuxurl"
-linux="$(basename "$linuxurl")"
-tar xf "$linux"
-cd "${linux-rpi%%\.*}"
-cp ../../configs/"$platform"-linux .config
-make INSTALL_HDR_PATH=../../rootfs headers_install
-cd ..
+[ -d "linux-$linuxver" ] || {
+	echo "installing linux headers..."
+	case "$platform" in
+		"rpi"*) linuxurl="$rpilinuxurl" ;;
+	esac
+	curl -O "$linuxurl"
+	linux="$(basename "$linuxurl")"
+	tar xf "$linux"
+	case "$platform" in
+		"rpi"*) cd "${linux-rpi%%\.*}" ;;
+		*) cd "linux-$linuxver" ;;
+	esac
+	cp ../../configs/"$platform"-linux.config .config
+	make INSTALL_HDR_PATH=../../rootfs headers_install
+	cd ..
+}
 
-echo "building libressl"
-curl -O "$libretlsurl"
-tar xf "$(basename "$libretlsurl")"
-libretls="$(basename "$libretlsurl")"
-cd "$libretls/"
-./configure --prefix /home/lord/distro/rootfs
-make
-cd ..
+[ -d "toybox-master" ] || {
+	echo "installing toybox..."
+	curl "$toyboxurl" > toybox.zip
+	unzip toybox.zip
+	cd toybox-master
+	cp ../../configs/"$platform"-toybox.config .config
+	make toybox
+	make PREFIX=../../rootfs/bin install_flat
+	cd ..
+}
 
-echo "installing toybox..."
-curl -O "$toyboxurl"
-toybox="$(basename "$toyboxurl")"
-cd "$toybox"
-cp ../../configs/"$platform"-toybox .config
-make CC="$CC -L ../$libretls" toybox
-make PREFIX=../../rootfs/bin install_flat
-cd ..
 
-echo "building mksh"
-curl -O "$mkshurl"
-tar xf "$(basename "$mkshurl")"
-cd mksh/
-chmod +x Build.sh 
-./Build.sh
-install -c -s -g bin -m 555 mksh ../rootfs/bin/sh
-cd ..
+[ -d "mksh" ] || {
+	echo "building mksh"
+	curl "$mkshurl" | tar xz
+	cd mksh/
+	chmod +x Build.sh 
+	./Build.sh
+	install -c -s -m 555 mksh ../../rootfs/bin/sh
+	install -c -m 444 lksh.1 mksh.1 ../../rootfs/share/man/man1/
+	cd ..
+}
 
 echo "runnning specific platform commands"
-../scripts/"post-install-$platform.sh"
+. ../scripts/"post-install-$platform.sh"
+
+cd ../
 
 echo "copying files"
-cp ../files/{passwd,issue,profile,shells,login.defs} ../rootfs/etc
-cp ../files/init ../rootfs/sbin
+mkdir rootfs/etc rootfs/sbin
+cp -r etc rootfs/etc
+cp files/init rootfs/sbin
 
+echo "copying extra packages"
+mkdir -p rootfs/share/pkg
+cp pkgs/* rootfs/share/pkg
 
 echo "compressing rootfs..."
-cd ../
 tar cf rootfs.tar.gz rootfs/*
